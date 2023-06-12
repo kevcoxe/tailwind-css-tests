273a72606906f25796b6880bd62b2182730dd5e8
Kevin Coxe <kevcoxe@gmail.com>
2023-06-12 10:29:37 -0400

    add something

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
 
   const postContent = Posts.map((post: PostInfoInterface, i: number) => {
     return (
-      <div key={i} className="col-span-5 lg:col-span-3 lg:col-start-2">
+      <div key={i} className="col-span-5 lg:col-span-3 lg:col-start-2 sm:max-h-full">
         <ImagePost postInfo={ post } />
       </div>
     )
